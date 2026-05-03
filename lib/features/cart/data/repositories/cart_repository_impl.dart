ab79498eec7877b0084ba0943b69f06502b10ba0
arisandibulan-boop <arisandibulan@gmail.com>
2026-05-03 08:37:50 +0700

    buat model pesanan dan enum status pesanan

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -1,82 +1,70 @@
-import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dio/dio.dart';
 import 'package:uas_katalogsaya/core/constants/api_constants.dart';
+import 'package:uas_katalogsaya/core/services/dio_client.dart';
 import 'package:uas_katalogsaya/features/cart/data/models/cart_model.dart';
 import 'package:uas_katalogsaya/features/cart/domain/repositories/cart_repository.dart';
 
 class CartRepositoryImpl implements CartRepository {
-  final Dio _dio = Dio();
-
-  CartRepositoryImpl() {
-    // Menambahkan Interceptor agar setiap request membawa Token Firebase
-    _dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) async {
-        final user = FirebaseAuth.instance.currentUser;
-        if (user != null) {
-          final token = await user.getIdToken();
-          options.headers['Authorization'] = 'Bearer $token';
-        }
-        return handler.next(options);
+  
+  // Fungsi pembantu untuk mengambil Token Firebase
+  Future<Options> _getHeaders() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final token = await user?.getIdToken();
+    return Options(
+      headers: {
+        'Authorization': 'Bearer $token',
       },
-    ));
+    );
   }
 
   @override
   Future<CartModel> getCart() async {
-    try {
-      final response = await _dio.get(ApiConstants.cart);
-      if (response.statusCode == 200) {
-        return CartModel.fromJson(response.data['data']);
-      }
-      throw Exception('Gagal mengambil data keranjang');
-    } catch (e) {
-      rethrow;
-    }
+    final options = await _getHeaders();
+    final response = await DioClient.instance.get(
+      ApiConstants.cart,
+      options: options,
+    );
+    final data = response.data['data'] as Map<String, dynamic>;
+    return CartModel.fromJson(data);
   }
 
   @override
   Future<void> addToCart(int productId, int quantity) async {
-    try {
-      await _dio.post(
-        ApiConstants.cart,
-        data: {
-          'product_id': productId,
-          'quantity': quantity,
-        },
-      );
-    } catch (e) {
-      rethrow;
-    }
+    final options = await _getHeaders();
+    await DioClient.instance.post(
+      ApiConstants.cart,
+      data: {'product_id': productId, 'quantity': quantity},
+      options: options,
+    );
   }
 
+  // FIXED: Menambahkan kembali fungsi yang hilang agar tidak error merah
   @override
   Future<void> updateCartItem(int cartItemId, int quantity) async {
-    try {
-      await _dio.put(
-        '${ApiConstants.cart}/$cartItemId',
-        data: {'quantity': quantity},
-      );
-    } catch (e) {
-      rethrow;
-    }
+    final options = await _getHeaders();
+    await DioClient.instance.put(
+      '${ApiConstants.cart}/$cartItemId',
+      data: {'quantity': quantity},
+      options: options,
+    );
   }
 
   @override
   Future<void> removeCartItem(int cartItemId) async {
-    try {
-      await _dio.delete('${ApiConstants.cart}/$cartItemId');
-    } catch (e) {
-      rethrow;
-    }
+    final options = await _getHeaders();
+    await DioClient.instance.delete(
+      '${ApiConstants.cart}/$cartItemId',
+      options: options,
+    );
   }
 
   @override
   Future<void> clearCart() async {
-    try {
-      await _dio.delete(ApiConstants.cart);
-    } catch (e) {
-      rethrow;
-    }
+    final options = await _getHeaders();
+    await DioClient.instance.delete(
+      ApiConstants.cart,
+      options: options,
+    );
   }
-}// Get Cart
-// Add Cart
+}
